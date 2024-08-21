e7573277c75877ea8b6034237e031b780ef47439
Chae-hyeong <hch5997@gmail.com>
2024-08-21 17:42:03 +0900

    feat: Update model in main.py

--- api/main.py
+++ api/main.py
@@ -3,13 +3,14 @@ from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from data.transforms import preprocess_text
 import torch
+from typing import Optional
 
 app = FastAPI()
 
-# 모델 및 토크나이저 로드 (학습된 모델 불러오기)
-model_path = "./checkpoints/best_model"  # 학습된 모델이 저장된 경로로 수정
-model = AutoModelForSequenceClassification.from_pretrained(model_path)
-tokenizer = AutoTokenizer.from_pretrained(model_path)
+# 모델 및 토크나이저 로드 (SafeTradeGuard_v1 모델 불러오기)
+model_name = "hyeongc/SafeTradeGuard_v2"
+model = AutoModelForSequenceClassification.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
 # 디바이스 설정 (GPU 사용 가능 시 GPU, 그렇지 않으면 CPU 사용)
 device = torch.device('mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu')
@@ -27,7 +28,7 @@ class ChatResponse(BaseModel):
     chat_room_id: int
     sender_id: int
     result: int
-    warning_message: str = None
+    warning_message: Optional[str] = None  # Optional[str]로 정의하여 선택적 필드로 설정
 
 # 메시지 예측 함수 (POST 요청 처리)
 @app.post("/predict", response_model=ChatResponse)
@@ -52,7 +53,7 @@ def predict_chat(chat: ChatRequest):
             result = logits.argmax(-1).item()
 
         # 결과에 따른 경고 메시지 설정
-        warning_message = None
+        warning_message = None  # 기본적으로 경고 메시지를 None으로 설정
         if result == 1:
             warning_message = "경고: 피싱 메시지로 의심됩니다."
         elif result == 2:
@@ -60,12 +61,12 @@ def predict_chat(chat: ChatRequest):
         elif result == 3:
             warning_message = "경고: 계좌번호가 포함되어 있습니다."
 
-        # 응답 생성
+        # 응답 생성 (warning_message가 None이면 해당 필드가 생략됨)
         return ChatResponse(
             chat_room_id=chat.chat_room_id,
             sender_id=chat.sender_id,
             result=result,
-            warning_message=warning_message
+            warning_message=warning_message  # None이면 필드가 반환되지 않음
         )
 
     except Exception as e:
