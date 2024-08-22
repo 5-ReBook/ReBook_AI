001d434913f3d8f2dcb6a4ba4880f7efebee82cd
suugit <69067257+suugit@users.noreply.github.com>
2024-08-22 14:37:55 +0900

    Revert "Deploy"

--- evaluate.py
+++ evaluate.py
@@ -1,34 +1,22 @@
 import pandas as pd
-from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
+from transformers import AutoTokenizer
 from data.dataset import CurseDataset
 from data.transforms import preprocess_text
-from utils.performance_metrics import compute_metrics
-import psutil
-import os
+from models.model import load_model
+from models.utils import compute_metrics
+from transformers import Trainer
 
-# 메모리 사용량 체크 함수
-def get_memory_usage():
-    process = psutil.Process(os.getpid())
-    mem_info = process.memory_info()
-    system_mem = psutil.virtual_memory()
-    
-    return {
-        "process_memory_MB": mem_info.rss / (1024 * 1024),  # 현재 프로세스 메모리 사용량 (MB)
-        "total_memory_MB": system_mem.total / (1024 * 1024),  # 시스템 전체 메모리 (MB)
-        "available_memory_MB": system_mem.available / (1024 * 1024),  # 사용 가능한 메모리 (MB)
-        "used_memory_MB": system_mem.used / (1024 * 1024),  # 시스템에서 사용 중인 메모리 (MB)
-    }
-
-# 테스트 데이터 로드 및 전처리
+# 데이터 로드 및 전처리
 test_file_path = './data/test_chat_dataset_v6.csv'
 test_df = pd.read_csv(test_file_path)
 test_df['text'] = test_df['text'].apply(preprocess_text)
 
 # 토크나이저 및 모델 로드
-tokenizer = AutoTokenizer.from_pretrained("hyeongc/SafeTradeGuard_v2")
-model = AutoModelForSequenceClassification.from_pretrained("hyeongc/SafeTradeGuard_v2")
+model_name = "beomi/KcELECTRA-base-v2022"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = load_model(model_name, num_labels=4)
 
-# 테스트 데이터 토크나이징 및 CurseDataset 생성
+# 토크나이징 및 CurseDataset 생성
 tokenized_test_sentences = tokenizer(
     list(test_df['text']),
     return_tensors="pt",
@@ -40,31 +28,8 @@ tokenized_test_sentences = tokenizer(
 test_label = test_df['target_label'].values
 test_dataset = CurseDataset(tokenized_test_sentences, test_label)
 
-# 평가 인자 설정
-training_args = TrainingArguments(
-    output_dir='./results',
-    per_device_eval_batch_size=16,
-    do_eval=True,
-    logging_dir='./logs',
-)
-
 # Trainer 설정
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    eval_dataset=test_dataset,
-    compute_metrics=compute_metrics  # 메트릭 계산 함수 추가
-)
+trainer = Trainer(model=model)
 
 # 평가 수행
-metrics = trainer.evaluate(eval_dataset=test_dataset)
-
-# 평가 결과 출력
-print(metrics)
-
-# 평가 완료 후 메모리 사용량 출력
-memory_usage = get_memory_usage()
-print(f"Final Memory Usage - Process Memory: {memory_usage['process_memory_MB']:.2f} MB, "
-      f"Total Memory: {memory_usage['total_memory_MB']:.2f} MB, "
-      f"Used Memory: {memory_usage['used_memory_MB']:.2f} MB, "
-      f"Available Memory: {memory_usage['available_memory_MB']:.2f} MB")
+trainer.evaluate(eval_dataset=test_dataset)
--- utils/performance_metrics.py
+++ utils/performance_metrics.py
@@ -1,5 +1,3 @@
-# utils/performance_metrics.py
-
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -18,16 +16,11 @@ def compute_metrics(pred):
     preds = pred.predictions.argmax(-1)
     precision, recall, f1, _ = precision_recall_fscore_support(labels, preds, average='micro')
     acc = accuracy_score(labels, preds)
-    
-    # 추가로 혼동 행렬을 계산할 수 있습니다.
-    conf_matrix = confusion_matrix(labels, preds)
-    
     return {
         'accuracy': acc,
         'f1': f1,
         'precision': precision,
-        'recall': recall,
-        'confusion_matrix': conf_matrix  # 혼동 행렬도 반환
+        'recall': recall
     }
 
 def plot_confusion_matrix(y_true, y_pred, labels):
