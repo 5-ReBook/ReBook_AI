ca1c79d56acb9eedfe53ceb2cc545a6526c82973
Chae-hyeong <hch5997@gmail.com>
2024-08-29 00:58:38 +0900

    refactor: Refactor dataset.py for improved code structure

--- data/dataset.py
+++ data/dataset.py
@@ -6,7 +6,7 @@ class CurseDataset(torch.utils.data.Dataset):
         self.labels = labels
 
     def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item = {key: val[idx].clone().detach() for key, val in self.encodings.items()}
         item["labels"] = torch.tensor(self.labels[idx])
         return item
 
